a17270f10daf6a1763f1ebccc87206be642ea7fd
fawl_03 <razidias@gmail.com>
2023-04-17 21:37:24 -0300

    fix: missing semicolon on race.ts

--- src/Races/Race.ts
+++ src/Races/Race.ts
@@ -2,16 +2,16 @@ export default abstract class Race {
   constructor(private _name: string, private _dexterity: number) { }
 
   get name(): string {
-    return this._name
+    return this._name;
   }
   
   get dexterity(): number {
-    return this._dexterity
+    return this._dexterity;
   }
 
   static createdRacesInstances(): number {
     throw new Error('Not implemented');
   }
   
-  abstract get maxLifePoints(): number
+  abstract get maxLifePoints(): number;
 }
